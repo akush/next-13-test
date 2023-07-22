7c16b35a37ad957065c5b5cf9ca9aebca1638991
Abhinav Kushwaha <abhinav@ipsator.com>
2023-07-22 14:43:49 +0530

    moved asset prefix to env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
+const { NEXT_PUBLIC_ASSET_PREFIX } = process.env;
 const nextConfig = {
-  assetPrefix: "https://ipay.ipsator.com"
+  assetPrefix: NEXT_PUBLIC_ASSET_PREFIX,
 }
 
 module.exports = nextConfig
